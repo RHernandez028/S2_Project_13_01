9b8d1de4cc4e7d3726ce6e2d11a0e082d835cca7
Raymond Hernandez <rhernandez028@west-mec.org>
2019-03-25 17:22:45 -0700

    Final Commit
    Finished and ready for review.

--- bw_review.js
+++ bw_review.js
@@ -44,7 +44,7 @@ function init() {
             stars[i].style.cursor = "pointer";
             stars[i].addEventListener("mouseenter", lightStars);
       }    
-      //document.getElementById("comment").addEventListener("keyup", updateCount);
+      document.getElementById("comment").addEventListener("keyup", updateCount);
 }
 
 //color star when the user moves the pointer over star image
@@ -63,17 +63,49 @@ function lightStars(e) {
       document.getElementById("rating").value = starNumber + " stars";
      
       //unlights the star after the images pointer moves off the star image
+      e.target.addEventListener("mouseleave", turnOffStars);
      
-      document.addEventListener("mouseleave", turnOffStars);
-     
-      //
-      document.addEventListener("mouseleave", function () {
-            document.removeEventListener("")
+      //when star has been clicked then the event listener will be removed
+      e. target.addEventListener("click", function() {
+            e.target.removeEventListener("mouseleave", turnOffStars);
       });
 }
 
+// unlight the star when the user moves the mouse pointer off the star image
+function turnOffStars() {
+      var stars = document.querySelectorAll("span#stars img"); 
+      
+      for (var i = 0; i < stars.length; i++) {
+            stars[i].src = "bw_star.png";      
+      }
+      document.getElementById("rating").value = "";
+}
 
+//keeps a running total of the number of characters that the user has typed
+function updateCount() {
 
+      // gives the value of the text area box to the variable named comments box
+      var commentText = document.getElementById("comment").value;
+
+      var charCount = countCharacters(commentText);
+      
+      var wordCountBox = document.getElementById("wordCount");
+
+      wordCountBox.value = charCount + "/1000";
+      
+      // displays the character count 
+      if (charCount > 1000) {
+            wordCount.style.color = "white";
+            wordCount.style.backgroundColor = "red";
+      } else {
+            wordCount.style.color = "black";
+            wordCount.style.baclgroundColor = "white";
+      }
+}
+
+function countCharacters(textStr) {
+
+}
   
 
 
